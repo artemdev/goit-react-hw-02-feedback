dcc1814f6922797dd46ef7f5d088394d1745bbfe
Artem Zymovets <artwayprojects@gmail.com>
2021-01-05 12:58:16 +0200

    mentor corrections

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -1,39 +1,40 @@
-import { Component } from 'react'
-import Section  from './section.js'
-import Notification from './notification.js'
+import { Component } from 'react';
+import Section from './section.js';
+import Notification from './notification.js';
 
 class Statistics extends Component {
-    countTotalFeedback = () => {
-        return this.props.state.good + this.props.state.bad + this.props.state.neutral
-      }
+  countTotalFeedback = () => {
+    let total = 0;
+    return (total +=
+      this.props.state.good + this.props.state.bad + this.props.state.neutral);
+  };
 
-    countPositiveFeedbackPercentage = () => {
-        return Math.round(this.props.state.good / this.countTotalFeedback() * 100) || 0
+  countPositiveFeedbackPercentage = () => {
+    return (
+      Math.round((this.props.state.good / this.countTotalFeedback()) * 100) || 0
+    );
+  };
+
+  render() {
+    const { neutral, bad, good } = this.props.state;
+    const totalFeedbacks = this.countTotalFeedback();
+
+    if (totalFeedbacks === 0) {
+      return <Notification message="No feedback given" />;
+    } else {
+      return (
+        <Section title="Statistics" className="statistics">
+          <div>Neutral {neutral}</div>
+          <div>Bad {bad}</div>
+          <div>Good! {good}</div>
+          Total: {totalFeedbacks}
+          <br />
+          Positive % : {this.countPositiveFeedbackPercentage()}
+          <br />
+        </Section>
+      );
     }
-
-    render() {
-        if(this.countTotalFeedback() <= 0) {
-            return(<Notification message="No feedback given" />)
-        }
-        else {
-            return(               
-                    <Section title="Statistics" className="statistics">
-                        <div>Neutral { this.props.state.neutral }</div>
-                        <div>Bad { this.props.state.bad }</div>
-                        <div>Good! { this.props.state.good }</div>
-                
-                    Total:  {this.countTotalFeedback()}
-                    <br />
-                    Positive % : {this.countPositiveFeedbackPercentage()}
-                    <br />
-                    </Section>
-
-                )
-        }
-        
-    }
+  }
 }
 
-export default Statistics
-
-
+export default Statistics;
